66810f55d8a72ecf6788c7a1eaa9f6b86f5dea92
sudatra <mail2sudatra@gmail.com>
2025-01-02 19:58:41 +0530

    added default tailwind classes and styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-3xl">Storage Manager</h1>
+    <div className="flex-center h-screen">
+      <h1 className="h1">Storage Manager</h1>
     </div>
   );
 }
