32bfce874b2780a311fcf1340c452217784bfd1a
sudatra <mail2sudatra@gmail.com>
2025-01-09 21:21:14 +0530

    added sign-in functionality

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -16,7 +16,7 @@ import {
 import { Input } from "@/components/ui/input"
 import Image from 'next/image'
 import Link from 'next/link'
-import { createAccount } from '@/lib/actions/user.actions'
+import { createAccount, signInUser } from '@/lib/actions/user.actions'
 import OTPModal from './OTPModal'
 
 type FormType = 'sign-in' | 'sign-up';
@@ -48,10 +48,12 @@ const AuthForm = ({ type }: { type: FormType }) => {
     setErrorMessage('');
 
     try {
-      const user = await createAccount({
-        fullName: values.fullName || '',
-        email: values.email
-      });
+      const user = type === 'sign-up' 
+      ? await createAccount({
+          fullName: values.fullName || '',
+          email: values.email
+        })
+      : await signInUser({ email: values.email })
   
       setAccountId(user.accountId)
     }
