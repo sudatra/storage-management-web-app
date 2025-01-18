fb12f66b40ab5555547ced1bae48405c73067d06
sudatra <mail2sudatra@gmail.com>
2025-01-18 12:24:43 +0530

    finished file-uploader component

--- components/FileUploader.tsx
+++ components/FileUploader.tsx
@@ -3,7 +3,7 @@
 import React, { useCallback, useState } from 'react'
 import { useDropzone } from 'react-dropzone'
 import { Button } from './ui/button'
-import { cn, getFileType } from '@/lib/utils'
+import { cn, convertFileToUrl, getFileType } from '@/lib/utils'
 import Image from 'next/image'
 import { Thumbnail } from './Thumbnail'
 
@@ -20,6 +20,13 @@ const FileUploader = ({ accountId, className }: Props) => {
     setFiles(acceptedFiles)
   }, [])
   const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+
+  const handleRemoveFile = (e: React.MouseEvent<HTMLImageElement, MouseEvent>, fileName: string) => {
+    e.stopPropagation();
+    setFiles((prevFiles) => prevFiles.filter((file) => (
+      file.name !== fileName
+    )));
+  }
 
   return (
     <div
@@ -54,8 +61,31 @@ const FileUploader = ({ accountId, className }: Props) => {
                     className='uploader-preview-item'
                   >
                     <div className='flex items-center gap-3'>
-                      <Thumbnail />
+                      <Thumbnail 
+                        type={type}
+                        extension={extension}
+                        url={convertFileToUrl(file)}
+                      />
+
+                      <div className='preview-item-name'>
+                        {file.name}
+
+                        <Image 
+                          src='/assets/icons/file-loader.gif'
+                          alt='loader'
+                          width={80}
+                          height={26}
+                        />
+                      </div>
                     </div>
+
+                    <Image 
+                      src='/assets/icons/remove.svg'
+                      alt='remove'
+                      width={24}
+                      height={24}
+                      onClick={(e) => handleRemoveFile(e, file.name)}
+                    />
                   </li>
                 )
               })
