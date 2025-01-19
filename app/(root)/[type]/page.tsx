05fdaa714ba30513a44fc8d58444d4fe56276ab2
sudatra <mail2sudatra@gmail.com>
2025-01-19 12:45:53 +0530

    added file fetching based on logged in user details

--- app/(root)/[type]/page.tsx
+++ app/(root)/[type]/page.tsx
@@ -1,13 +1,49 @@
+import { Sort } from '@/components/Sort';
+import { getFiles } from '@/lib/actions/file.actions';
+import { Models } from 'node-appwrite';
 import React from 'react'
 
 const Page = async ({ params }: SearchParamProps) => {
   const type = ((await params)?.type as string) || "";
 
+  const files = await getFiles();
+
   return (
     <div className='page-container'>
       <section className='w-full'>
         <h1 className='h1'>{type}</h1>
+
+        <div className='total-size-section'>
+          <p className='body-1'>
+            Total: <span className='h5'>0 MB</span>
+          </p>
+
+          <div className='sort-container'>
+            <p className='body-1 hidden sm:block text-light-200'>Sort by:</p>
+
+            <Sort />
+          </div>
+        </div>
       </section>
+
+      {
+        files.total > 0 ? (
+          <section className='file-list'>
+            {
+              files.documents.map((file: Models.Document) => (
+                <h1 
+                  className='h1'
+                  key={file.$id}
+                >
+                  {file.name}
+                </h1>
+              ))
+            }
+          </section>
+        ) : (
+          <p className='empty-list'>No files uploaded</p>
+        )
+      }
     </div>
   )
 }
---
+++ components/Sort.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Sort = () => {
+  return (
+    <div>
+      Sort
+    </div>
+  )
+}
