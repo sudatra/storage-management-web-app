5ab1314f0ea2f70727eca459e5846e4246bb81f6
Sudatra Ghosh <sudatraghosh@jmangroup.com>
2025-01-22 14:54:49 +0530

    fixed middleware setup and application launch error throw

--- middleware.ts
+++ middleware.ts
@@ -2,8 +2,15 @@ import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(request: NextRequest) {
   const url = request.nextUrl.clone();
+  const requestedUrl = request.nextUrl.pathname.replace(/^\//, '');
   const isAuthPage: boolean = ((url.pathname === '/sign-in') || (url.pathname === '/sign-up'));
   const hasSession: boolean = request.cookies.has('appwrite-session');
+
+  if(requestedUrl === '/') {
+    if(!hasSession) {
+      return NextResponse.redirect('/sign-in');
+    }
+  }
 
   if(isAuthPage && !hasSession) {
     return NextResponse.next();
