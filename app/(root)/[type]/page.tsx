86f57068b641dedc67e9242320cb2d510574d433
sudatra <mail2sudatra@gmail.com>
2025-01-25 19:13:22 +0530

    added sort and search functionality

--- app/(root)/[type]/page.tsx
+++ app/(root)/[type]/page.tsx
@@ -6,11 +6,16 @@ import { getFileTypesParams } from '@/lib/utils';
 import { Models } from 'node-appwrite';
 import React from 'react'
 
-const Page = async ({ params }: SearchParamProps) => {
+const Page = async ({ searchParams, params }: SearchParamProps) => {
   const type = ((await params)?.type as string) || "";
   const types = getFileTypesParams(type) as FileType[];
+  const searchText = ((await searchParams)?.query as string) || '';
+  const sort = ((await searchParams)?.sort as string) || '';
 
-  const files = await getFiles({ types: types });
+  console.log(types)
+
+
+  const files = await getFiles({ types: types, searchText, sort });
 
   return (
     <div className='page-container dark:!bg-[#1a1c20]'>
