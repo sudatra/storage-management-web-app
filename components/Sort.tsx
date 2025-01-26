57625a3cc1751329369249064ee8424e7bb2b53f
sudatra <mail2sudatra@gmail.com>
2025-01-26 12:36:02 +0530

    added sorting functionality

--- components/Sort.tsx
+++ components/Sort.tsx
@@ -1,9 +1,46 @@
+'use client'
+
 import React from 'react'
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+import { usePathname, useRouter } from 'next/navigation'
+import { sortTypes } from '@/constants'
 
 export const Sort = () => {
+  const router = useRouter();
+  const path = usePathname();
+
+  const handleSort = (value: string) => {
+    router.push(`${path}?sort=${value}`);
+  }
+
   return (
-    <div className='dark:!text-white'>
-      Sort
-    </div>
+    <Select 
+      onValueChange={handleSort}
+      defaultValue={sortTypes[0].value}
+    >
+      <SelectTrigger className="sort-select dark:!bg-black dark:!text-white">
+        <SelectValue placeholder={sortTypes[0].value} />
+      </SelectTrigger>
+
+      <SelectContent className='sort-select-content'>
+        {
+          sortTypes.map((sort) => (
+            <SelectItem
+              key={sort.label}
+              className='shad-select-item'
+              value={sort.value}
+            >
+              {sort.label}
+            </SelectItem>
+          ))
+        }
+      </SelectContent>
+    </Select>
   )
 }
