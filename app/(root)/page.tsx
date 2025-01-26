81e8f8497306dc41d114afefabb9db5c43181ad7
sudatra <mail2sudatra@gmail.com>
2025-01-26 13:02:43 +0530

    added data fetching for dashboard

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,11 @@
+import { getFiles, getTotalSpaceUsed } from "@/lib/actions/file.actions";
 
-export default function Home() {
+export default async function Home() {
+  const [files, totalSpace] = await Promise.all([
+    getFiles({ types: [], limit: 10 }),
+    getTotalSpaceUsed()
+  ]);
+
   return (
     <div className="flex-center h-screen">
       <h1 className="h1 dark:!text-white">Storage Manager</h1>
